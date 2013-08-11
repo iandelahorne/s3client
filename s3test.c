461fe0484781b84f1bb08eef39e163a7ddfc42de
Ian Delahorne <ian.delahorne@gmail.com>
2013-08-12 00:41:43 +0200

    Numerous updates
    Use LibXML with XPath for parsing the result and putting it into structs
    Added s3_get operation
    Split out date creation into a separate function
    Split out the actual CURL op into a separate function

--- s3test.c
+++ s3test.c
@@ -9,6 +9,11 @@
 #include <curl/curl.h>
 
 #include <expat.h>
+#include <libxml/parser.h>
+#include <libxml/tree.h>
+#include <libxml/xpath.h>
+#include <libxml/xpathInternals.h>
+
 #define NS_DELIM '.'
 
 #include "s3.h"
@@ -20,7 +25,7 @@
  * 
  */
 
-struct s3_contents {
+struct s3_content {
 	char *key;
 	char *lastmod; /* time_t */
 	size_t size;
@@ -30,6 +35,16 @@ struct s3_contents {
 struct s3_xml_data {
 	int level;
 };
+#define S3_SECRET_LENGTH 128
+#define S3_ID_LENGTH 128
+
+struct S3 {
+	char *secret;
+	char *id;
+	char *base_url;
+};
+
+# if 0
 
 static void XMLCALL
 s3_start_tag(void *user_data, const XML_Char *name, const XML_Char **attrs) {
@@ -37,7 +52,7 @@ s3_start_tag(void *user_data, const XML_Char *name, const XML_Char **attrs) {
 	struct s3_xml_data *data = user_data;
 	const char *nsdelim = strrchr(name, NS_DELIM);
 	int i;	
-	char kbuf[1024];
+	/* char kbuf[1024]; */
 
 	if (nsdelim) {
 		tagns = strndup(name, nsdelim - name);
@@ -114,7 +129,7 @@ static void xml_do_stuff(char *str) {
 	XML_ParserFree(parser);
 	free(data);
 }
-
+#endif 
 char *
 hmac_sign(const char *key, const char *str, size_t len) {
 	unsigned char *digest; 
@@ -161,24 +176,19 @@ hmac_sign(const char *key, const char *str, size_t len) {
 	return buf;
 }
 
-#define S3_SECRET_LENGTH 128
-#define S3_ID_LENGTH 128
-
-struct S3 {
-	char *secret;
-	char *id;
-};
-
 
 static struct S3 *
-s3_init(const char *id, const char *secret) {
+s3_init(const char *id, const char *secret, const char *base_url) {
 	struct S3 *s3 = malloc(sizeof (struct S3));
 
 	s3->id = malloc(S3_ID_LENGTH);
 	s3->secret = malloc(S3_SECRET_LENGTH);
+	/* XXX better length */
+	s3->base_url = malloc(256); 
 	
 	strlcpy(s3->id, id, S3_ID_LENGTH);
 	strlcpy(s3->secret, secret, S3_SECRET_LENGTH);
+	strlcpy(s3->base_url, base_url, 255);
 
 	return s3;
 }
@@ -187,35 +197,169 @@ static void
 s3_free(struct S3 *s3) {
 	free(s3->id);
 	free(s3->secret);
+	free(s3->base_url);
 	
 	free(s3);
 }
 
-/* http://stackoverflow.com/questions/2329571/c-libcurl-get-output-into-a-string */
-
-int main (int argc, char **argv) {
-	struct S3 *s3; 
-	struct s3_string *str;
+void
+register_namespaces(xmlXPathContextPtr ctx, const xmlChar *nsList) {
+	xmlChar* nsListDup;
+	xmlChar* prefix;
+	xmlChar* href;
+	xmlChar* next;
 	
-#ifdef AMZN
-	s3 = s3_init("REDACTED", "REDACTED");
-	const char *bucket = "REDACTED";
+	nsListDup = xmlStrdup(nsList);
+	if(nsListDup == NULL) {
+		fprintf(stderr, "Error: unable to strdup namespaces list\n");
+		return;
+	}
 	
-#else
-	s3 = s3_init("O5GQYKFKYGPFLB7E0MI4", "10c7hqOBkH2AqcmzNmTjFSz-fgNZHndKrzj9Kg==");
-	const char *bucket = "test-bucket";
-#endif	
-	const char *method = "GET";
+	next = nsListDup; 
+	while(next != NULL) {
+		/* skip spaces */
+		while((*next) == ' ') next++;
+		if((*next) == '\0') break;
+		
+		/* find prefix */
+		prefix = next;
+		next = (xmlChar*)xmlStrchr(next, '=');
+		if(next == NULL) {
+			fprintf(stderr,"Error: invalid namespaces list format\n");
+			xmlFree(nsListDup);
+			return;
+		}
+		*(next++) = '\0';	
+		
+		/* find href */
+		href = next;
+		next = (xmlChar*)xmlStrchr(next, ' ');
+		if(next != NULL) {
+			*(next++) = '\0';	
+		}
+		
+		/* do register namespace */
+		if(xmlXPathRegisterNs(ctx, prefix, href) != 0) {
+			fprintf(stderr,"Error: unable to register NS with prefix=\"%s\" and href=\"%s\"\n", prefix, href);
+			xmlFree(nsListDup);
+			return;
+		}
+	}
+	
+	xmlFree(nsListDup);
+}
 
+struct s3_content *
+parse_content(xmlNode *root) {
+	struct s3_content *content = malloc(sizeof(struct s3_content));
+	xmlNode *node = NULL;
+	for (node = root; node; node = node->next) {
+		if (node->type == XML_ELEMENT_NODE) {
+			xmlChar *value =  xmlNodeGetContent(node->children);
+			size_t len = strlen((const char *) value);
+
+			if (strcasecmp("key", (const char *)node->name) == 0) {
+				content->key = malloc(len + 2);
+				strlcpy(content->key, (const char *)value, len + 1);
+			} else if (strcasecmp("lastmodified", (const char *)node->name) == 0) {
+				content->lastmod = malloc(len + 2);
+				strlcpy(content->lastmod, (const char *)value, len + 1);
+			} else if (strcasecmp("etag", (const char *)node->name) == 0) {
+				content->etag = malloc(len + 2);
+				strlcpy(content->etag, (const char *)value, len + 1);
+			} else {
+				
+				printf("node type: Element, name: %s\n", node->name);
+				printf("node xmlsNodeGetContent: %s\n",value);
+			}
+			xmlFree(value);
+		}
+	}
+	return content;
+	
+}
+/* 
+void
+libxml_walk_nodes(xmlNode *root) {
+	xmlNode *node = NULL;
+	for (node = root; node; node = node->next) {
+		if (node->type == XML_ELEMENT_NODE) {
+			printf("node type: Element, name: %s\n", node->name);
+		}
+		libxml_walk_nodes(node->children);
+	}
+}
+*/
+
+void 
+walk_xpath_nodes(xmlNodeSetPtr nodes) {
+	xmlNodePtr cur;
+	int size;
+	int i;
+
+	size = (nodes) ? nodes->nodeNr : 0;
+	printf("size is %d nodes\n", size);
+	for (i = 0; i < size ; i++) {
+		if (nodes->nodeTab[i]->type == XML_ELEMENT_NODE) {
+			cur = nodes->nodeTab[i];
+			
+			/* Push this onto a list of contents, return list */
+			struct s3_content *content = parse_content(cur->children);
+		}
+	}
+}
+/* Should change this to a callback */
+void 
+execute_xpath_expr(const xmlDocPtr doc, const xmlChar *xpath_expr, const xmlChar *ns_list) {
+	xmlXPathContextPtr xpath_ctx;
+	xmlXPathObjectPtr xpath_obj;
+
+	xpath_ctx = xmlXPathNewContext(doc);
+	if (xpath_ctx == NULL) {
+		fprintf(stderr,"Error: unable to create new XPath context\n");
+	}
+
+	register_namespaces(xpath_ctx, ns_list);
+	
+	xpath_obj = xmlXPathEvalExpression(xpath_expr, xpath_ctx);
+	if(xpath_obj == NULL) {
+		fprintf(stderr,"Error: unable to evaluate xpath expression \"%s\"\n", xpath_expr);
+	}
+	walk_xpath_nodes(xpath_obj->nodesetval);
+
+	xmlXPathFreeObject(xpath_obj);
+	xmlXPathFreeContext(xpath_ctx); 
+}
+
+void 
+libxml_do_stuff(char *str) {
+	xmlDocPtr doc;
+	doc = xmlReadMemory(str, strlen(str), "noname.xml", NULL, 0);
+	/* xmlNode *root_element = xmlDocGetRootElement(doc); */
+
+	/* Since Amazon uses an XML Namespace, we need to declare it and use it as a prefix in Xpath queries, even though it's  */
+	execute_xpath_expr(doc, (const xmlChar *)"//amzn:Contents", (const xmlChar *)"amzn=http://s3.amazonaws.com/doc/2006-03-01/");
+	
+	xmlFreeDoc(doc);
+}
+
+
+
+static void
+s3_list_bucket(struct S3 *s3, const char *bucket) {
 	char *date;
 	char *data, *digest;
 	char *hdr;
-
 	time_t now;
 	struct tm tm;
-
+	
+	char *url;
+	
 	CURL *curl;
 	struct curl_slist *headers = NULL;
+	struct s3_string *str;
+	const char *method = "GET";
+
 
 	str = s3_string_init();		
 	date = malloc(128);
@@ -223,15 +367,15 @@ int main (int argc, char **argv) {
 	tm = *gmtime(&now);
 	strftime(date, 128, "%a, %d %b %Y %H:%M:%S %Z", &tm);
 
-	
 	data = malloc(1024);
-	snprintf(data, 1023, "%s\n\n\n%s\n/%s/", method, date, bucket);
-	
+	snprintf(data, 1023, "%s\n\n\n%s\n/%s/", method, date, bucket);	
 	fprintf(stderr, "data is %s\n", data);
 
 	digest = hmac_sign(s3->secret, data, strlen(data));
-	
 	fprintf(stderr, "Authentication: AWS %s:%s\n", s3->id, digest);
+	
+	asprintf(&url, "http://%s.%s/?delimiter=/", bucket, s3->base_url);
+	fprintf(stderr, "url is %s\n", url);
 
 	curl = curl_easy_init();
 	
@@ -251,13 +395,14 @@ int main (int argc, char **argv) {
 	curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1);
 
 	curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+
+	/* http://stackoverflow.com/questions/2329571/c-libcurl-get-output-into-a-string */
+
 	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, s3_string_curl_writefunc);
 	curl_easy_setopt(curl, CURLOPT_WRITEDATA, str);
 
-#ifdef AMZN
-	curl_easy_setopt(curl, CURLOPT_URL, "http://REDACTED.s3.amazonaws.com/?delimiter=/");
-#else	
-	curl_easy_setopt(curl, CURLOPT_URL, "http://test-bucket.s3.amazonaws.com/?delimiter=/");
+	curl_easy_setopt(curl, CURLOPT_URL, url);
+#if 0
 	curl_easy_setopt(curl, CURLOPT_PROXY, "http://localhost:8080");
 #endif
 	curl_easy_perform(curl);
@@ -265,12 +410,115 @@ int main (int argc, char **argv) {
 	curl_easy_cleanup(curl);
 	curl_slist_free_all(headers);
 
-	xml_do_stuff(str->ptr);	
+	libxml_do_stuff(str->ptr);	
 	printf("%s\n", str->ptr);
 	s3_string_free(str);
-	s3_free(s3);
 	free(data);
 	free(date);
-	free(digest);
+	free(digest);	
+}
+
+/* Add return values later */
+void
+s3_perform_op(struct S3 *s3, const char *method, const char *url, const char *sign_data, const char *date, struct s3_string *str) {
+	char *digest;
+	char *hdr;
+	
+	CURL *curl;
+	struct curl_slist *headers = NULL;
+
+
+
+	fprintf(stderr, "data to sign:%s\n", sign_data);
+
+	digest = hmac_sign(s3->secret, sign_data, strlen(sign_data));
+	/* fprintf(stderr, "Authentication: AWS %s:%s\n", s3->id, digest); */
+	
+	curl = curl_easy_init();
+	
+	hdr = malloc(1024);
+
+	snprintf(hdr, 1023, "Date: %s", date);
+	headers = curl_slist_append(headers, hdr);
+
+	snprintf(hdr, 1023, "Authorization: AWS %s:%s", s3->id, digest);
+	headers = curl_slist_append(headers, hdr);
+	free(hdr);
+	
+	
+	curl_easy_setopt(curl, CURLOPT_VERBOSE, 1);
+/*	curl_easy_setopt(curl, CURLOPT_HEADER, 1); */
+	curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1);
+
+	curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+
+	/* http://stackoverflow.com/questions/2329571/c-libcurl-get-output-into-a-string */
+
+	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, s3_string_curl_writefunc);
+	curl_easy_setopt(curl, CURLOPT_WRITEDATA, str);
+
+	curl_easy_setopt(curl, CURLOPT_URL, url);
+#if 0
+	curl_easy_setopt(curl, CURLOPT_PROXY, "http://localhost:8080");
+#endif
+	curl_easy_perform(curl);
+
+	curl_easy_cleanup(curl);
+	curl_slist_free_all(headers);
+
+	free(digest);	
+}
+
+char *
+s3_make_date() {
+	char *date;
+	time_t now;
+	struct tm tm;
+
+	date = malloc(128);
+	now = time(0);
+	tm = *gmtime(&now);
+	strftime(date, 128, "%a, %d %b %Y %H:%M:%S %Z", &tm);
+
+	return date;
+
+}
+static void
+s3_get(struct S3 *s3, const char *bucket, const char *key) {
+	const char *method = "GET";
+	char *sign_data;
+	char *date;
+	char *url;
+	struct s3_string *str;
+
+	date = s3_make_date();
+
+	asprintf(&sign_data, "%s\n\n\n%s\n/%s/%s", method, date, bucket, key);	
+	asprintf(&url, "http://%s.%s/%s", bucket, s3->base_url, key);
+	str = s3_string_init();		
+	
+	s3_perform_op(s3, method, url, sign_data, date, str);
+	printf("%d\n", str->len);
+	
+	s3_string_free(str);
+
+	free(sign_data);
+	free(date);
+	free(url);
+}
+
+
+int main (int argc, char **argv) {
+	struct S3 *s3; 
+	
+	s3 = s3_init("REDACTED", "REDACTED", "s3.amazonaws.com");
+	const char *bucket = "REDACTED";
+	
+	/* s3_list_bucket(s3, bucket); */
+	
+	s3_get(s3, bucket, "Towel-Dog.jpg");
+
 	return 0;
+	s3_free(s3);
+
 }
