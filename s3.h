9f48cbdea7b7a8bef7cb909d70abbdf4e12f905c
Ian Delahorne <ian.delahorne@gmail.com>
2013-08-20 21:11:59 +0200

    Moved functions to s3ops.c
    Moved s3_make_date, s3_get, s3_delete, s3_put and s3_peform_op to their
    own file.

--- s3.h
+++ s3.h
@@ -34,6 +34,15 @@ struct s3_string {
 };
 
 
+struct S3 {
+	char *secret;
+	char *id;
+	char *base_url;
+	char *proxy;
+};
+
+
+
 struct s3_string * s3_string_init();
 size_t s3_string_curl_writefunc(void *ptr, size_t len, size_t nmemb, struct s3_string *s);
 size_t s3_string_curl_readfunc(void *ptr, size_t len, size_t nmemb, struct s3_string *s);
@@ -42,5 +51,12 @@ void s3_string_free(struct s3_string *str);
 char * s3_hmac_sign(const char *key, const char *str, size_t len);
 char * s3_md5_sum(const char *content, size_t len);
 
+char * s3_make_date();
+
+void s3_perform_op(struct S3 *s3, const char *method, const char *url, const char *sign_data, const char *date, struct s3_string *out, struct s3_string *in, const char *content_md5, const char *content_type);
+
+void s3_get(struct S3 *s3, const char *bucket, const char *key, struct s3_string *out);
+void s3_delete(struct S3 *s3, const char *bucket, const char *key);
+void s3_put(struct S3 *s3, const char *bucket, const char *key, const char *content_type, const char *data, size_t len);
 
 #endif /* _S3_H */
