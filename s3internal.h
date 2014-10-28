6515b96a22a179a49a7d8cc5014bc98b4a24bfe9
Ian Delahorne <ian.delahorne@gmail.com>
2014-10-27 23:50:50 -0600

    Use foo(void) instead of foo()

--- s3internal.h
+++ s3internal.h
@@ -1,7 +1,7 @@
 #ifndef _S3_INTERNAL_H
 #define _S3_INTERNAL_H
 
-char * s3_make_date();
+char * s3_make_date(void);
 void s3_perform_op(struct S3 *s3, const char *method, const char *url, const char *sign_data, const char *date, struct s3_string *out, struct s3_string *in, const char *content_md5, const char *content_type);
 
 #endif //_S3_INTERNAL_H
